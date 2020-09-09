c4198198c4ccf2b622ca072ae05aa65a2fafd8b9
zhouyangok <zhouyangx99@163.com>
2020-09-09 22:54:25 +0800

    遗漏

---
+++ cloud-api-commons/src/main/java/com/springcloud/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.springcloud.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName MybatisPlusConfig
+ * @Description: mybatis-plus分页配置
+ * @Author zhouyang
+ * @Date 2020/9/9 下午10:41.
+ */
+
+@Configuration
+@ConditionalOnClass(value = {PaginationInterceptor.class})
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
