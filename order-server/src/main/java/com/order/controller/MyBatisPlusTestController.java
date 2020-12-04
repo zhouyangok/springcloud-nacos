e1ab61f35fcf6970e839237a41be12196cc142b0
zhouyangok <zhouyangx99@163.com>
2020-12-04 21:45:50 +0800

    join方法添加

--- order-server/src/main/java/com/order/controller/MyBatisPlusTestController.java
+++ order-server/src/main/java/com/order/controller/MyBatisPlusTestController.java
@@ -1,8 +1,14 @@
 package com.order.controller;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 import io.swagger.annotations.Api;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName MyBatisPlusTestController
@@ -14,4 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mybatisPlus")
 public class MyBatisPlusTestController {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("a1");
+        list.add("a2");
+        list.add("a3");
+        String ab = Convert.toStr(list);
+        String join1 = StrUtil.join(",", list);
+        System.out.println(join1);
+        System.out.println(ab);//[a1, a2, a3]
+        //        =============
+        String join = StringUtils.join(list.toArray(), ",");
+        System.out.println(join);
+
+    }
 }
